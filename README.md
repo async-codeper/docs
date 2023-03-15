8b0efc36da0803f10d045875e69e064db0ca855c
fangjiebiao <fangjiebiao@migu.cn>
2023-03-15 14:16:39 +0800

    de

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # 参考
-[Github Pages](https://pages.github.com/)
-[Vuepress部署](https://v2.vuepress.vuejs.org/zh/guide/deployment.html#github-pages)
-[Github CI](https://github.com/crazy-max/ghaction-github-pages)
+- [Github Pages](https://pages.github.com/)
+- [Vuepress部署](https://v2.vuepress.vuejs.org/zh/guide/deployment.html#github-pages)
+- [Github CI](https://github.com/crazy-max/ghaction-github-pages)
---
+++ commmitlint.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'chore',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'eslint',
+                'mock',
+                'comments',
+                'merge'
+            ]
+        ]
+    }
+}
